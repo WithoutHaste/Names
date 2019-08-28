a6d20aac9dae162c1bf83ab3637d0b6f55a85238
J M <wohaste@gmail.com>
2019-08-28 16:29:12 -0600

    getting started on the service

---
+++ NameContracts/INameService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace NameContracts
+{
+	[ServiceContract]
+	public interface INameService
+	{
+		[OperationContract]
+		string GetData(int value);
+	}
+}
