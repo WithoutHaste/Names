290599e80a3525820dc5251cd726f03543ee7937
J M <wohaste@gmail.com>
2019-09-04 18:30:42 -0600

    NameWebsite getting 404 error when calling service operation

---
+++ NameWebsite/NameWebsite/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NameWebsite.NameService;
+
+namespace NameWebsite.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			NameClient nameClient = new NameClient();
+			string data = nameClient.GetData(35);
+			nameClient.Close();
+			
+			return View(data);
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+}
---
+++ NameWebsite/NameWebsite/NameService/NameClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+using NameContracts;
+
+namespace NameWebsite.NameService
+{
+	public class NameClient : ClientBase<INameService>, INameService
+	{
+		public NameClient() : base("NameEndpoint")
+		{
+		}
+
+		public string GetData(int value)
+		{
+			return Channel.GetData(value);
+		}
+	}
+}
