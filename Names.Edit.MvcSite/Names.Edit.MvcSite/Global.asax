cc30c11a6c13c211abaac372149480a16fab1706
J M <wohaste@gmail.com>
2019-09-14 15:23:58 -0600

    edit site: initial checkin

---
+++ Names.Edit.MvcSite/Names.Edit.MvcSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Names.Edit.MvcSite.MvcApplication" Language="C#" %>
---
+++ Names.Edit.MvcSite/Names.Edit.MvcSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Names.Edit.MvcSite.Startup))]
+namespace Names.Edit.MvcSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
