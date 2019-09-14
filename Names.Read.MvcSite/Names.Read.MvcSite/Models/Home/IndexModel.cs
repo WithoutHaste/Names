1ded2d1f81bfc10f1c4ac346866128a422dc0a48
J M <wohaste@gmail.com>
2019-09-14 10:51:59 -0600

    bug fix: re-selecting "All" from front end had 0 results

--- Names.Read.MvcSite/Names.Read.MvcSite/Models/Home/IndexModel.cs
+++ Names.Read.MvcSite/Names.Read.MvcSite/Models/Home/IndexModel.cs
@@ -11,7 +11,7 @@ namespace Names.Read.MvcSite.Models.Home
 		public CategoryModel[] Categories {
 			set {
 				origins = new List<OriginModel>();
-				origins.Add(new OriginModel(null, "All"));
+				origins.Add(new OriginModel("", "All")); //if value is set to null, it defaults to "on" on the webpage
 				if(String.IsNullOrEmpty(selectedOrigin))
 					origins[0].Checked = true;
 				ConvertCategoriesToOrigins(value);
@@ -22,6 +22,12 @@ namespace Names.Read.MvcSite.Models.Home
 		public OriginModel[] Origins { get { return origins.ToArray(); } }
 
 		private string selectedOrigin;
+
+		public string NameCountMessage {
+			get {
+				return String.Format("{0:n0} Name{1} Found", Names.Length, Names.Length == 1 ? "" : "s");
+			}
+		}
 
 		public IndexModel(string selectedOrigin)
 		{
