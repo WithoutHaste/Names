051124d16009f5fac1c8702fd72f5b190e0dfc8b
J M <wohaste@gmail.com>
2019-09-14 15:37:39 -0600

    edit site: display a page of names

--- Names.Edit.MvcSite/Names.Edit.MvcSite/Controllers/HomeController.cs
+++ Names.Edit.MvcSite/Names.Edit.MvcSite/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Names.Edit.MvcSite.ServiceClients;
+using Names.Edit.SoapService.Contracts;
 
 namespace Names.Edit.MvcSite.Controllers
 {
@@ -10,21 +12,11 @@ namespace Names.Edit.MvcSite.Controllers
 	{
 		public ActionResult Index()
 		{
-			return View();
-		}
+			NameClient client = new NameClient();
+			NameDetailResponse[] result = client.GetPagedNames(0, 50);
+			client.Close();
 
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
-
-			return View();
-		}
-
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
-
-			return View();
+			return View("Index", result);
 		}
 	}
 }
---
+++ Names.Edit.MvcSite/Names.Edit.MvcSite/ServiceClients/NameClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+using Names.Edit.SoapService.Contracts;
+
+namespace Names.Edit.MvcSite.ServiceClients
+{
+	public class NameClient : ClientBase<INamesEditService>, INamesEditService
+	{
+		public NameClient() : base("NameEndpoint")
+		{
+		}
+
+		public NameDetailResponse[] GetPagedNames(int pageIndex, int rowsPerPage)
+		{
+			return Channel.GetPagedNames(pageIndex, rowsPerPage);
+		}
+	}
+}
