92dc4b7255baa2df0d89ba4b3b45f19254b218df
J M <wohaste@gmail.com>
2021-04-10 11:42:23 -0600

    continuing conversion - failing to run on linux

--- Names.Read.SoapService.Contracts/CategoryResponse.cs
+++ Names.Read.SoapService.Contracts/CategoryResponse.cs
@@ -1,18 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Names.Read.SoapService.Contracts
+﻿namespace Names.Read.SoapService.Contracts
 {
-	[DataContract]
 	public class CategoryResponse
 	{
-		[DataMember]
 		public string Category { get; set; }
-		[DataMember]
 		public CategoryResponse[] SubCategories { get; set; }
 	}
 }
--- Names.Read.SoapService.Contracts/Enums.cs
+++ Names.Read.SoapService.Contracts/Enums.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Names.Read.SoapService.Contracts
+﻿namespace Names.Read.SoapService.Contracts
 {
-	[DataContract]
 	public enum GenderOption : int
 	{
-		[EnumMember]
 		Any = 0,
-		[EnumMember]
 		OnlyBoys = 1,
-		[EnumMember]
 		OnlyGirls = 2
 	};
 }
